e80e5084694fcca4dd458e0195781ba4817ac351
James Mittler II <jamesmittlerii@my.smccd.edu>
2025-12-20 21:04:07 -0800

    Delete first boot breadcrumb from 01-run.sh
    Remove first boot breadcrumb creation.

--- stage2/19-sfz-router/01-run.sh
+++ stage2/19-sfz-router/01-run.sh
@@ -35,7 +35,5 @@ python3 -c "import jack, mido; print('[sfz-router] python imports OK')" || true
 test -f /opt/router/load_single.py
 echo "[sfz-router] Installed."
 
-# leave a breakcrumb saying we are in our first boot
-touch /etc/firstboot-pending
 CHROOT_EOF
 
