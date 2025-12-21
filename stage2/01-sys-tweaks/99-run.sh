898ca03461b6046ed312ddea29a7bf56d6ee4f8f
James Mittler II <jamesmittlerii@my.smccd.edu>
2025-12-20 20:21:51 -0800

    Retrieve current git branch and commit in run.sh
    Add host-side git information retrieval to script

--- stage2/01-sys-tweaks/99-run.sh
+++ stage2/01-sys-tweaks/99-run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -e
 
+# Host-side git info
+PG_BRANCH=$(git rev-parse --abbrev-ref HEAD 2>/dev/null || echo unknown)
+PG_COMMIT=$(git rev-parse --short HEAD 2>/dev/null || echo unknown)
+
+
 on_chroot <<EOF
 cat >/etc/pi-gen-build <<EOM
 BUILD_NAME=${IMG_NAME:-custom-pi-gen}
