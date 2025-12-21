cf4057316df37c749af7120ee48cfed33e805ff5
James Mittler II <jamesmittlerii@my.smccd.edu>
2025-12-20 22:47:57 -0800

    Update git branch and commit variables in run.sh

--- stage2/01-sys-tweaks/99-run.sh
+++ stage2/01-sys-tweaks/99-run.sh
@@ -9,7 +9,7 @@ on_chroot <<EOF
 cat >/etc/pi-gen-build <<EOM
 BUILD_NAME=${IMG_NAME:-custom-pi-gen}
 BUILD_DATE=$(date -u +%Y-%m-%dT%H:%M:%SZ)
-GIT_BRANCH=$(cd /pi-gen && git rev-parse --abbrev-ref HEAD 2>/dev/null || echo unknown)
-GIT_COMMIT=$(cd /pi-gen && git rev-parse --short HEAD 2>/dev/null || echo unknown)
+GIT_BRANCH=${PG_BRANCH}
+GIT_COMMIT=${PG_COMMIT}
 EOM
 EOF
